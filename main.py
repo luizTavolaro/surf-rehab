927d840615863dfea94d579da42933507bf54dd3
luizTavolaro <luiz.felipetaa@gmail.com>
2024-11-20 18:05:06 -0300

    feat: speed

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-
-
 import pygame
 from pygame.locals import *
 import random
 
 pygame.init()
 
-SCREEN_WIDTH = 700
+SCREEN_WIDTH = 768
 SCREEN_HEIGHT = 432
-
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 bg_images = []
@@ -21,6 +18,18 @@ sea = pygame.image.load(f"img/sea.png").convert_alpha()
 sea = pygame.transform.scale(sea, (SCREEN_WIDTH, SCREEN_HEIGHT))
 background = pygame.image.load(f"img/background.png").convert_alpha()
 background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+sun2 = pygame.image.load(f"img/sun2.png").convert_alpha()
+sun2 = pygame.transform.scale(sun2, (SCREEN_WIDTH, SCREEN_HEIGHT))
+sun3 = pygame.image.load(f"img/sun3.png").convert_alpha()
+sun3 = pygame.transform.scale(sun3, (SCREEN_WIDTH, SCREEN_HEIGHT))
+background2 = pygame.image.load(f"img/background2.png").convert_alpha()
+background2 = pygame.transform.scale(background2, (SCREEN_WIDTH, SCREEN_HEIGHT))
+background3 = pygame.image.load(f"img/background3.png").convert_alpha()
+background3 = pygame.transform.scale(background3, (SCREEN_WIDTH, SCREEN_HEIGHT))
+clouds2 = pygame.image.load(f"img/clouds2.png").convert_alpha()
+clouds2 = pygame.transform.scale(clouds2, (SCREEN_WIDTH, SCREEN_HEIGHT))
+clouds3 = pygame.image.load(f"img/clouds3.png").convert_alpha()
+clouds3 = pygame.transform.scale(clouds3, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 bg_images.append(clouds)
 bg_images.append(sun)
@@ -35,6 +44,7 @@ BOTTOM_LIMT = SCREEN_HEIGHT - 80
 LANE_HEIGHT = (BOTTOM_LIMT - TOP_LIMT) // 3
 
 FONT = pygame.font.Font(None, 36)
+BIG_FONT = pygame.font.Font(None, 72)
 
 LANE_Y_POSITIONS = [
     TOP_LIMT,
@@ -42,6 +52,58 @@ LANE_Y_POSITIONS = [
     TOP_LIMT + 2 * LANE_HEIGHT,
     BOTTOM_LIMT
 ]
+
+# Cores
+WHITE = (255, 255, 255)
+BLUE = (50, 150, 255)
+
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(text, True, color)
+    text_rect = text_obj.get_rect(center=(x, y))
+    surface.blit(text_obj, text_rect)
+
+def start_screen():
+    running = True
+    while running:
+        SCREEN.fill(BLUE)
+        draw_text("Surf Game", BIG_FONT, WHITE, SCREEN, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4)
+        draw_text("Pressione ENTER para Iniciar", FONT, WHITE, SCREEN, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        draw_text("Pressione ESC para Sair", FONT, WHITE, SCREEN, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 40)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:  # ENTER para iniciar
+                    running = False
+                elif event.key == K_ESCAPE:  # ESC para sair
+                    pygame.quit()
+                    exit()
+
+        pygame.display.flip()
+
+def pause_screen():
+    paused = True
+    while paused:
+        SCREEN.fill((0, 0, 0))  # Fundo preto
+        draw_text("Jogo Pausado", BIG_FONT, WHITE, SCREEN, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 3)
+        draw_text("Pressione ENTER para Continuar", FONT, WHITE, SCREEN, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        draw_text("Pressione ESC para Sair", FONT, WHITE, SCREEN, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 40)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:  # ENTER para continuar
+                    paused = False
+                elif event.key == K_ESCAPE:  # ESC para sair
+                    pygame.quit()
+                    exit()
+
+        pygame.display.flip()
+
 
 class Surfer(pygame.sprite.Sprite):
     def __init__(self, name):
@@ -66,13 +128,13 @@ class Surfer(pygame.sprite.Sprite):
         self.rect.center = self.position
 
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, lane_y):
+    def __init__(self, lane_y, w, h):
         super().__init__()
         self.images = [
             pygame.image.load(f"img/seagul1.png").convert_alpha(),
             pygame.image.load(f"img/seagul2.png").convert_alpha()
         ]
-        self.images = [pygame.transform.scale(img, (30, int(LANE_HEIGHT * 0.6))) for img in self.images]
+        self.images = [pygame.transform.scale(img, (w, int(h))) for img in self.images]
         self.image = self.images[0]
         self.rect = self.image.get_rect()
         self.rect.x = SCREEN_WIDTH
@@ -93,15 +155,38 @@ class Obstacle(pygame.sprite.Sprite):
             self.animation_index = (self.animation_index + 1) % len(self.images)
             self.image = self.images[self.animation_index]
 
-def draw_bg(scroll = 0):
-    SCREEN.blit(background, (0, 0))
-    SCREEN.blit(sun, (SCREEN_WIDTH - sun.get_width(), 0))
+def draw_bg(level=1, scroll=0, speed=1):
+    if level == 1:
+        sun_img = sun
+        background_img = background
+        clouds_img = clouds
+        scroll = 100
+        speed = 1
+        
+    elif level == 2:
+        sun_img = sun2
+        background_img = background2
+        clouds_img = clouds2
+        scroll = 200
+        speed = 2
+
+    elif level == 3:
+        sun_img = sun3
+        background_img = background3
+        clouds_img = clouds3
+        scroll = 300
+        speed = 3
+
+    SCREEN.blit(background_img, (0, 0))
+    SCREEN.blit(sun_img, (SCREEN_WIDTH - sun_img.get_width(), 0))
     for x in range(50):
-        SCREEN.blit(clouds, ((x * bg_width) - scroll * 1, 0))
-        SCREEN.blit(sea, ((x * bg_width) - scroll * 2, 0))
+        SCREEN.blit(clouds_img, ((x * bg_width) - scroll * 1 * speed, 0))
+        SCREEN.blit(sea, ((x * bg_width) - scroll * 2 * speed, 0))
 
 
 def main():
+    start_screen()
+    level = 1
     surfer = Surfer("Luiz")
     all_sprites = pygame.sprite.Group()
     all_sprites.add(surfer)
@@ -112,6 +197,8 @@ def main():
     obstacle_interval = 2000
     obstacle_passed_count = 0
     obstacles = pygame.sprite.Group()
+    obstacle_w = 30
+    obstacle_h = LANE_HEIGHT * 0.4
 
     clock = pygame.time.Clock()
     running = True
@@ -120,6 +207,9 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:  # ESC para pausar
+                    pause_screen()
         
         if scroll == max_scroll:
             max_scroll += 100
@@ -131,7 +221,7 @@ def main():
 
             lanes = random.sample(LANE_Y_POSITIONS[:-1], 2)
             for lane_y in lanes:
-                obstacle = Obstacle(lane_y + LANE_HEIGHT // 2)
+                obstacle = Obstacle(lane_y + LANE_HEIGHT // 2, obstacle_w, obstacle_h)
                 obstacles.add(obstacle)
                 all_sprites.add(obstacle)
 
@@ -143,13 +233,24 @@ def main():
                 obstacle_passed_count += 1
                 obstacle.kill()
 
-        draw_bg(scroll=scroll)
+        draw_bg(level=level, scroll=scroll)
         all_sprites.update()
-        all_sprites.draw(SCREEN)    
+        all_sprites.draw(SCREEN) 
 
-        score_text = FONT.render(f"{obstacle_passed_count // 2}", True, (0, 0, 0))
-        SCREEN.blit(score_text, (10, 10))
-        
+
+        score_color = (0,0,0)
+        if obstacle_passed_count // 2 >= 3:
+            level = 3
+        elif obstacle_passed_count // 2 >= 2: 
+            level = 2
+        if level == 3:
+            score_color = (255,255,255)
+
+        level_text = FONT.render(f"Nível: {level}", True, score_color)
+        score_text = FONT.render(f"{obstacle_passed_count // 2}", True, score_color)
+        SCREEN.blit(level_text, (10, 10))
+        SCREEN.blit(score_text, (10, 40))
+
         pygame.display.flip()
         clock.tick(60)
   
